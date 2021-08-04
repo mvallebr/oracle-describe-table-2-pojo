89872f87e7394d8ce8f0688d35d6c4bce55740b0
Marcelo Elias Del Valle <mvallebr@gmail.com>
2021-08-04 13:53:11 +0100

    fix test failure

--- describe2pojo.py
+++ describe2pojo.py
@@ -6,11 +6,25 @@ def underscore_2_camel_case(underscore_file_name):
     return ''.join(x.capitalize() or '_' for x in underscore_file_name.split('_'))
 
 
+def field_list(describe_table_text):
+    lines = describe_table_text.split("\n")
+    col_names = lines[0].split()
+    separators = [lines[1].find(" ")]
+    separators.append(lines[1].find(" ", separators[-1]+1))
+    result = []
+    for line in lines[2:]:
+        field_name = line[0:separators[0]]
+        nullable = line[separators[0]+1:separators[1]]
+        field_type = line[separators[1]+1:]
+        result.append((field_name, nullable, field_type))
+    return result
+
+
 def convert_to_pojo(describe_table_text, class_name):
     return f"""public class {class_name} {{
 
-        }}
-        """
+}}
+"""
 
 
 def parse_arguments():
@@ -19,22 +33,23 @@ def parse_arguments():
         description="""Oracle Describe table 2 pojo""")
     parser.add_argument('input_file_name', type=str,
                         help='The file containing oracle describe table output')
-    parser.add_argument('output_file_name', type=str,
+    parser.add_argument('output_folder', type=str,
                         help='The place where we will save the pojo file')
     parser.add_argument('--overwrite', default=False, action='store_true')
     return parser.parse_args()
 
 
-def main(input_file_name, output_file_name, overwrite):
+def main(input_file_name, output_folder, overwrite):
     with open(input_file_name, "r") as f:
         describe_table_text = f.read()
 
+    class_name = underscore_2_camel_case(
+        os.path.splitext(os.path.basename(input_file_name))[0])
+
+    output_file_name = os.path.join(output_folder, f"{class_name}.java")
     if os.path.exists(output_file_name) and (not overwrite or os.path.isdir(output_file_name)):
         raise Exception(
-            "Either output file exists and overwrite flag wasn't set, or it's a folder.")
-
-    class_name = underscore_2_camel_case(
-        os.path.splitext(os.path.basename(output_file_name))[0])
+            f"{output_file_name} either exists and overwrite flag wasn't set, or it's a folder.")
 
     pojo_text = convert_to_pojo(describe_table_text, class_name)
 
--- tests/test_describe2pojo.py
+++ tests/test_describe2pojo.py
@@ -1,29 +1,29 @@
 from describe2pojo import main
 import pytest
-import os 
+import os
 
 DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
 TMP_FOLDER = os.path.join(os.path.dirname(__file__), "tmp")
+os.makedirs(TMP_FOLDER)
 
 test_data = [
-    ("usage_writers.txt", "UsageWriters.java", False, "UsageWritersExpected.java"),
-    ("usage_chain_steps.txt", "UsageChainSteps.java", False, "UsageChainStepsExpected.java"),
-    ("usage_writer_chains.txt", "UsageWriterChains.java", False, "UsageWriterChainsExpected.java"),
+    ("usage_writers.txt",   False, "UsageWriters.java"),
+    ("usage_chain_steps.txt",  False, "UsageChainSteps.java"),
+    ("usage_writer_chains.txt",  False, "UsageWriterChains.java"),
 ]
 
 
-@pytest.mark.parametrize(argnames="input_file,output_file,overwrite,expected_output", argvalues=test_data)
-def test_main(input_file,output_file,overwrite,expected_output):
+@pytest.mark.parametrize(argnames="input_file,overwrite,expected_output", argvalues=test_data)
+def test_main(input_file, overwrite, expected_output):
     with open(os.path.join(DATA_FOLDER, expected_output), "r") as f:
         expected_output_text = f.read().strip()
-         
+
     input_file_full_path = os.path.join(DATA_FOLDER, input_file)
-    output_file_full_path = os.path.join(TMP_FOLDER, output_file)
-    main(input_file_full_path, output_file_full_path, overwrite) 
 
-    with open(os.path.join(DATA_FOLDER, output_file_full_path), "r") as f:
+    main(input_file_full_path, TMP_FOLDER, overwrite)
+
+    actual_output_file_full_path = os.path.join(TMP_FOLDER, expected_output)
+    with open(os.path.join(DATA_FOLDER, actual_output_file_full_path), "r") as f:
         actual_output_text = f.read().strip()
-    
-    assert actual_output_text == expected_output_text
 
- 
+    assert actual_output_text == expected_output_text, f"\n`{actual_output_text}`\n\n!=\n\n`{expected_output_text}`"
